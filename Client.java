67ca86314ca8920c25e9d9e61b258324be335dac
takuma1127 <40846958+takuma1127@users.noreply.github.com>
2019-07-23 14:04:39 +0900

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,183 @@
+import java.util.ArrayList;
+
+
+
+public class Client {
+
+	public static void main(String[] args) {
+
+		Suuchi s = new Suuchi();
+
+		Observer o1 = new NishinHyouji();
+
+		Observer o2 = new JyuurokushinHyouji();
+
+
+
+		s.attach(o1);
+
+		s.attach(o2);
+
+		int i = 0;
+
+		while (i < 100) {
+
+			s.putValue(i);
+
+			i += (int) (Math.random() * 30) - 10;
+
+		}
+
+	}
+
+}
+
+
+
+abstract class Subject {
+
+	public ArrayList<Observer> observers;
+
+
+
+	Subject() {
+
+		observers = new ArrayList<Observer>();
+
+	}
+
+
+
+	public void attach(Observer o) {
+
+		observers.add(o);
+
+
+
+	}
+
+
+
+	public void detach(Observer o) {
+
+		observers.remove(o);
+
+	}
+
+
+
+	public void tsuuchi() {
+
+		for (Observer observer : observers) {
+
+			observer.update(this);
+
+		}
+
+	}
+
+}
+
+
+
+class Suuchi extends Subject {
+
+	int suuchiState;
+
+	int atai;
+
+
+
+	public int getState() {
+
+		return suuchiState;
+
+	}
+
+
+
+	public void putValue(int atai) {
+
+		if (atai > this.atai) {
+
+			this.atai = atai;
+
+			this.tsuuchi();
+
+		} else { // 本来ここから3行は不要となる行て゛す。
+
+			System.out.println("確認用メッセーシ゛:更新してません");
+
+		}
+
+	}
+
+
+
+	public int getValue() {
+
+		return atai;
+
+	}
+
+}
+
+
+
+interface Observer {
+
+	public void update(Subject s);
+
+}
+
+
+
+class NishinHyouji implements Observer {
+
+	public void update(Subject s) {
+
+		print(((Suuchi) s).getValue());
+
+	}
+
+
+
+	private void print(int n) {
+
+		System.out.println(n + "を2進数て゛表示します");
+
+		//
+
+		// ここに2進数表示処理を作成する
+
+		//
+
+	}
+
+}
+
+
+
+class JyuurokushinHyouji implements Observer {
+
+	public void update(Subject s) {
+
+		print(((Suuchi) s).getValue());
+
+	}
+
+
+
+	private void print(int n) {
+
+		System.out.println(n + "を 16 進数て゛表示します");
+
+		//
+
+		// ここに 16 進数表示処理を作成する
+
+		//
+
+	}
+
+}
